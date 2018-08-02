821ea4e976f0b30f882e902a7acf18811c2fa671
Lucídio Andrade Barbosa deSouza <lucidiosouza@cidao.local>
2018-08-02 10:46:34 -0400

    Initial Commit

---
+++ Teste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teste
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 02/08/2018.
+//  Copyright © 2018 Lucídio Andrade Barbosa de Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
